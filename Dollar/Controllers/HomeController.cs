cbd55e1f77c506d3da7627dcbc662ab51a047c54
Vipin <v.krishnadas.nair@accenture.com>
2021-11-02 21:17:35 +0530

    initial commit
    initial commit

---
+++ Dollar/Controllers/HomeController.cs
@@ -0,0 +1,90 @@
+﻿using Dollar.Models;
+using Dollar.Models.Home;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Dollar.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpPost]
+        public ActionResult Upload(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return Content("file not selected");
+
+            var path = Path.Combine(
+                        Directory.GetCurrentDirectory(), "uploads", file.FileName);
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                file.CopyToAsync(stream);
+            }
+
+            List<CsvFields> csvSeperator = System.IO.File.ReadAllLines(path)
+                                           .Skip(1)
+                                           .Select(v => CsvFields.FromCsv(v))
+                                           .ToList();
+            return View(csvSeperator);
+        }
+        [HttpPost]
+        public ActionResult UploadDatatable(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return Content("file not selected");
+
+            var path = Path.Combine(
+                        Directory.GetCurrentDirectory(), "uploads", file.FileName);
+
+            using (var stream = new FileStream(path, FileMode.Create))
+            {
+                file.CopyToAsync(stream);
+            }
+
+            List<CsvFields> data = System.IO.File.ReadAllLines(path)
+                                           .Skip(1)
+                                           .Select(v => CsvFields.FromCsv(v))
+                                           .ToList();
+
+            //string jsonString = JsonSerializer.Serialize(csvSeperator);
+            //string data = "{\"data\":" + jsonString + "}";
+            //return Json(data);
+            return Json(new { jsonData=data });
+        }
+        public IActionResult Upload()
+        {
+            List<CsvFields> csvSeperator = new List<CsvFields>();
+            return View(csvSeperator);
+        }
+        public IActionResult UploadDatatable()
+        {
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
+}
---
+++ Dollar/Models/Home/CsvFields.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dollar.Models.Home
+{
+    public class CsvFields
+    {
+        [Key]
+        public string Store_id { get; set; }
+        public string Store_name { get; set; }
+        public string Store_phone { get; set; }
+
+
+        public static CsvFields FromCsv(string csvLine)
+        {
+            string[] values = csvLine.Split(',');
+            CsvFields csvValues = new CsvFields();
+            csvValues.Store_id = values[0];
+            csvValues.Store_name = values[1];
+            csvValues.Store_phone = values[2];
+            return csvValues;
+        }
+
+    }
+}
